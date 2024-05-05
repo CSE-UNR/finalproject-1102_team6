6f4f1c6d0b1d3d8df154a73184e6fd0499f52189
Diego Lopez <diegolopez@nevada.unr.edu>
2024-05-05 06:39:34 +0000

    added save and crop

--- imageProcesser.c
+++ imageProcesser.c
@@ -122,7 +122,73 @@ void loadImage(/*pass in array rows cols and img[][] to change */){
  
  }
  
+//---------------------------------------------------------
+
+void cropImage(int col, int row, int arr[][col]){
+
+	int newLeft, newRight, newTop, newBottom;
+	
+	printf("The image you want to crop is %d x %d.", col, row);
+	printf("\nThe row and column values start in the upper lefthand corner.\n");
+	
+	printf("Which column do you want to be the new left side? ");
+	scanf("%d", &newLeft);
+	while(newLeft<1 && newleft>col){
+		printf("Invalid column value. Choose a value between 1 and %d ", col);
+		scanf("%d", &newLeft);
+	}
+
+	printf("\nWhich column do you want to be the new right side? ");
+	scanf("%d", &newRight);
+	while(newRight<=newLeft && newRight>col){
+		printf("Invalid column value. Choose a value between %d and %d ",newLeft, col);
+		scanf("%d", &newLeft);
+	}
+	
+	printf("\nWhich row do you want to be the new top? ");
+	scanf("%d", &newTop);
+	while(newTop<1 && newTop>row){
+		printf("Invalid row value. Choose a value between 1 and %d", row);
+		scanf("%d", &newTop);
+	}
+
+	printf("\nWhich row do you want to be the new bottom? ");
+	scanf("%d", &newBottom);
+	while(newBottom<=newTop && newBottom>row){
+		printf("Invalid row value. Choose a value between %d and %d ",newTop, row);
+		scanf("%d", &newBottom);
+	}
+	
+	int crpImg[newRight][newBottom]
+	
+	for(int r=0;r<newBottom;r++){
+		for(int c=0;c<newRight;c++){
+			newImage[r][c]= arr[newLeft-1][newTop-1];
+		}
+	}
+	
+	displayImage(newRight,newBottom,cropImg[][newBottom];
+	return 0;
+}
+
+//---------------------------------------------------------
  
+ void saveImage(int col, int row, int arr[][col]){
+ 
+ 	char option;
+ 	char nameSave[30]
+ 	
+ 	printf("Would you like to save? (y/n) ");
+ 	scanf(" &c", &option);
+ 	
+ 	if(option='y'){
+ 		printf("\nWhat do you want to name the image file? (include the extension) "
+ 		fgets(nameSave, 30, stdin);
+ 		FILE* file = fopen(nameSave, "w");
+ 		
+ 	}
+ 
+ }
  
  
  
